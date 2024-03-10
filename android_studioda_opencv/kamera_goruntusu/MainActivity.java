ea434c1163bec6f25d6c2ee1108913e119ece040
Oguz Kagan Donmez <oguzkagandonmez01@gmail.com>
2024-03-10 18:46:47 +0300

    android_studioda_opencv klasoru guncellendi.

---
+++ android_studioda_opencv/kamera_goruntusu/MainActivity.java
@@ -0,0 +1,98 @@
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import org.opencv.android.CameraActivity;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Mat;
+
+import java.util.Collections;
+import java.util.List;
+
+public class MainActivity extends CameraActivity {
+
+    CameraBridgeViewBase cameraBridgeViewBase;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getPermission();
+
+        cameraBridgeViewBase = findViewById(R.id.cameraView);
+
+        cameraBridgeViewBase.setCvCameraViewListener(new CameraBridgeViewBase.CvCameraViewListener2() {
+            @Override
+            public void onCameraViewStarted(int width, int height) {
+
+            }
+
+            @Override
+            public void onCameraViewStopped() {
+
+            }
+
+            @Override
+            public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+            
+                return inputFrame.rgba();
+            }
+        });
+
+        if(OpenCVLoader.initDebug()){
+            Log.d("LOADED","success");
+
+            cameraBridgeViewBase.enableView();//Bu fonksiyon onCreate()'de en bastayken program hata veriyordu buraya tasiyinca duzeldi.
+        }else{
+            Log.d("LOADED","error");
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        cameraBridgeViewBase.enableView();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        cameraBridgeViewBase.disableView();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        cameraBridgeViewBase.disableView();
+    }
+
+    @Override
+    protected List<? extends CameraBridgeViewBase> getCameraViewList() {
+        
+        //return super.getCameraViewList(); Bu fonksiyonu donduruyordu silip alttakini yazdik.
+        return Collections.singletonList(cameraBridgeViewBase);
+    }
+
+    private void getPermission() {
+        if(checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            requestPermissions(new String[]{Manifest.permission.CAMERA}, 101);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if(grantResults.length > 0 && grantResults[0] != PackageManager.PERMISSION_GRANTED){
+            getPermission();
+        }
+    }
+}
